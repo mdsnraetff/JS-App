a0a40c179704a21edaf2ffc164f9d0817b703216
Madison <96503969+mdsnraetff@users.noreply.github.com>
2022-09-14 19:37:09 -0400

    pokemonList addition

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello World');
-
-let favoriteFood ="Potato";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    abilities: ['Chlorophyll', 'Overgrow']
+  },
+  {
+    name: "Sandslash",
+    height: 1,
+    abilities: ['Sand-veil', 'Sand-rush']
+  },
+  {
+    name: "Paras",
+    height: 0.3,
+    abilities: ['Damp', 'Effect-spore']
+  }
+];
