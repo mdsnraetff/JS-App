afbbc07bf5e698f466e0a19ec40ab8ec5c4c8a93
Madison <96503969+mdsnraetff@users.noreply.github.com>
2022-09-21 14:19:58 -0400

    Parenthesis for height

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,8 @@ let pokemonList = [
 //Pokemon Forloop//
 for (let i=0; i < pokemonList.length; i++)
   if(pokemonList[i].height > 0.7){
-    document.write(pokemonList[i].name+' ' + 'height: ' + pokemonList[i].height + ' - What a big guy!' + '<br>');
+    document.write(pokemonList[i].name+' ' + '(height: ' + pokemonList[i].height + ') - What a big guy!' + '<br>');
   }
   else {
-    document.write(pokemonList[i].name + ' ' + 'height: ' + pokemonList[i].height + '<br>');
+    document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')<br>');
   }
